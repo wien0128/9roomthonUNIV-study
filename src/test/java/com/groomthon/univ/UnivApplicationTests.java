9f56ace9d257044e523cc20797b7359408734416
Hyunbin Kim <taque88@naver.com>
2025-04-04 23:56:02 +0900

    init

---
+++ src/test/java/com/groomthon/univ/UnivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groomthon.univ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
